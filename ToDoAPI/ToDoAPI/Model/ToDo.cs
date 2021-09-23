5c5d7b6d67a37c884094c3639936ef3b66a2a8bc
JunsooKimCicomDesktop <hughkim@cicomprogram.com>
2021-09-23 16:50:45 -0700

    add - Net Core Backend API server

---
+++ ToDoAPI/ToDoAPI/Model/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoAPI.Model
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string ToDoName { get; set; }
+        public bool ToDoStatus { get; set; } = false;
+        public string ToDoDescription { get; set; }
+
+
+    }
+}
---
+++ ToDoAPI/ToDoAPI/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoAPI.Model
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDo> TodoItems { get; set; }
+    }
+}
