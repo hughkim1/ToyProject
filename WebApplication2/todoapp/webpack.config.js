e41660f248453cb83ad8e8c58f0472b23a1a0f65
JunsooKimCicomDesktop <hughkim@cicomprogram.com>
2021-09-24 10:42:15 -0700

    add- webpack-config settings

---
+++ WebApplication2/todoapp/webpack.config.js
@@ -0,0 +1,62 @@
+﻿// Webpack uses this to work with directories
+const path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let VueLoaderPlugin = require('vue-loader/lib/plugin');
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+
+    // Path to your entry point. From this file Webpack will begin its work
+    entry: './src/main.js',
+
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        // This loader resolves url() and @imports inside CSS
+                        loader: "css-loader",
+                    }
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin()
+    ],
+    resolve: {
+        alias: {
+            vue$: "vue/dist/vue.runtime.esm.js",
+        },
+        extensions: ["*", ".js", ".vue", ".json"],
+    },
+
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on the final bundle. For now, we don't need production's JavaScript 
+    // minifying and other things, so let's set mode to development
+    mode: 'development'
+};
